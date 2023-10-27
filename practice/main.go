69001ac10f0cf3b7e41508e3d4b8358d0e66eb1f
RaghurajTarikere <raghuraj.tp1992@gmail.com>
2023-10-27 10:24:45 +0530

    Practising decoding the JSON string in Go

--- practice/main.go
+++ practice/main.go
@@ -1,29 +1,74 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			MaxVersion: tls.VersionTLS12,
+		},
+	}
+	client := &http.Client{Transport: tr}
 
-	resp, err := http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Response type: %T\n", resp)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.69")
 
-	defer resp.Body.Close()
-
-	bytes, err := ioutil.ReadAll(resp.Body)
+	response, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 
-	content := string(bytes)
-	fmt.Print(content)
+	fmt.Printf("Type of response is: %T\n", response)
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	tours := decodeFromJson(string(body))
+
+	for _, tour := range tours {
+		fmt.Println("The tour name is:", tour.Name)
+		fmt.Println("The price of the tour is:", tour.Price)
+	}
+}
+
+type Tour struct {
+	Name, Price string
+}
+
+func decodeFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	if err != nil {
+		panic(err)
+	}
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		if err != nil {
+			panic(err)
+		}
+		tours = append(tours, tour)
+	}
+
+	return tours
 }
