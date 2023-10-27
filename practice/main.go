25005c6c82155be86462fc947412d9be7f928d8d
RaghurajTarikere <raghuraj.tp1992@gmail.com>
2023-10-27 09:45:41 +0530

    Practising reading a text file from web

--- practice/main.go
+++ practice/main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			MaxVersion: tls.VersionTLS12,
+		},
+	}
+	client := &http.Client{Transport: tr}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.69")
+
+	response, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Type of response is: %T\n", response)
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("The read data is ", string(body))
 }
